c321fe8f11d5b2bc43b9ad6f5d60951009bad7fa
Aya Shahin <66850776+AyaMShahin@users.noreply.github.com>
2023-01-19 03:28:41 +0200

    second commit

--- script.js
+++ script.js
@@ -1,14 +1,14 @@
-// const imgDiv = document.querySelector(".imgs");
-// const imgs = document.querySelectorAll(".hero");
-// const image = this;
-// imgDiv.addEventListener("mousemove", moving);
+const hero1 = document.querySelector(".hero1"),
+  hero2 = document.querySelector(".hero2"),
+  hero3 = document.querySelector(".hero3"),
+  hero4 = document.querySelector(".hero4"),
+  root = document.documentElement;
 
-// function moving(e) {
-//   imgs.forEach(function (move) {
-//     let movingValue = move.getAttribute("data-value");
-//     let moveX = e.clientX - movingValue;
-//     let moveY = e.clientY - movingValue;
-//     move.style.transform =
-//       "translateX(" + moveX + "px) translateY(" + moveY + "px)";
-//   });
-// }
+root.addEventListener("mousemove", (e) => {
+  const x = e.clientX,
+    y = e.clientY;
+  hero1.style.transform = `translate(${x / 40}px,${y / 50}px)`;
+  hero2.style.transform = `translate(${x / 25}px,${y / 50}px)`;
+  hero3.style.transform = `translate(${x / 15}px,${y / 50}px)`;
+  hero4.style.transform = `translate(${x / 10}px,${y / 50}px)`;
+});
