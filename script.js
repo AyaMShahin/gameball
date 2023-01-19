8dd6a5ed8bebbca4afadf0f956a9b450c7fecdb6
Aya Shahin <66850776+AyaMShahin@users.noreply.github.com>
2023-01-19 17:43:16 +0200

    slide up effect added

--- script.js
+++ script.js
@@ -1,9 +1,12 @@
+// The image moves just slightly according to where I move my mouse.
+
 const hero1 = document.querySelector(".hero1"),
   hero2 = document.querySelector(".hero2"),
   hero3 = document.querySelector(".hero3"),
   hero4 = document.querySelector(".hero4"),
   root = document.documentElement;
 
+// To achieve this parallax effect, you can apply different ratios to the elements.
 root.addEventListener("mousemove", (e) => {
   const x = e.clientX,
     y = e.clientY;
@@ -12,3 +15,45 @@ root.addEventListener("mousemove", (e) => {
   hero3.style.transform = `translate(${x / 15}px,${y / 50}px)`;
   hero4.style.transform = `translate(${x / 10}px,${y / 50}px)`;
 });
+
+let section = document.querySelectorAll("section");
+let menu = document.querySelectorAll("header nav a");
+
+window.onscroll = () => {
+  section.forEach((i) => {
+    let top = window.scrollY;
+    let offset = i.offsetTop - 150;
+    let height = i.offsetHeight;
+    let classs = i.getAttribute("class");
+
+    if (top >= offset && top < offset + height) {
+      menu.forEach((link) => {
+        link.classList.remove("active");
+        // document
+        //   .querySelector("header nav ul a[href*=" + classs + "]")
+        //   .classList.add("active");
+      });
+    }
+  });
+};
+
+function reveal() {
+  var reveals = document.querySelectorAll(".reveal");
+
+  for (var i = 0; i < reveals.length; i++) {
+    var windowHeight = window.innerHeight;
+    var elementTop = reveals[i].getBoundingClientRect().top;
+    var elementVisible = 150;
+
+    if (elementTop < windowHeight - elementVisible) {
+      reveals[i].classList.add("active");
+    } else {
+      reveals[i].classList.remove("active");
+    }
+  }
+}
+
+window.addEventListener("scroll", reveal);
+
+// To check the scroll position on page load
+reveal();
